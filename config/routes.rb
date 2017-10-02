615ad4c8218aa40ffe192f342b0f6e97e01eec75
san4es113 <san4es113@gmail.com>
2017-10-02 21:26:07 +0300

    add new page n/y

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'passlosts#homepage'
+  get 'notexists' => 'passlosts#notexist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
